256b8eb1a0960a1d6e5e6b9be5945fa43cd5911a
merzainc <h200649z@hit.ac.zw>
2024-07-03 09:55:37 +0200

    feat: test headings on dark

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,45 +7,12 @@ function IndexPage() {
         <title>TSX</title>
       </Head>
       <div className='mx-auto max-w-7xl pt-8 px-4'>
-        <h3 className='heading-md font-bold mb-3'>The King's Plan</h3>
-        <div className='space-y-3'>
-          <p className='text-regular'>
-            The king thought long and hard, and finally came up with a brilliant
-            plan.
-          </p>
-          <p className='text-secondary'>
-            The king thought long and hard, and finally came up with a brilliant
-            plan.
-          </p>
-          <p className='text-tertiary'>
-            The king thought long and hard, and finally came up with a brilliant
-            plan.
-          </p>
-          <p className='text-muted'>
-            The king thought long and hard, and finally came up with a brilliant
-            plan.
-          </p>
-          <p className='text-link'>
-            The king thought long and hard, and finally came up with a brilliant
-            plan.
-          </p>
-          <p className='text-success'>
-            The king thought long and hard, and finally came up with a brilliant
-            plan.
-          </p>
-          <p className='text-danger'>
-            The king thought long and hard, and finally came up with a brilliant
-            plan.
-          </p>
-          <p className='text-info'>
-            The king thought long and hard, and finally came up with a brilliant
-            plan.
-          </p>
-          <p className='text-warning'>
-            The king thought long and hard, and finally came up with a brilliant
-            plan.
-          </p>
-        </div>
+        <h1 className='heading-2xl font-bold mb-3'>The King's Plan</h1>
+        <h2 className='heading-xl font-bold mb-3'>The King's Plan</h2>
+        <h3 className='heading-lg font-bold mb-3'>The King's Plan</h3>
+        <h4 className='heading-md font-bold mb-3'>The King's Plan</h4>
+        <h5 className='heading-sm font-bold mb-3'>The King's Plan</h5>
+        <h6 className='heading-xs font-bold mb-3'>The King's Plan</h6>
       </div>
     </>
   );
